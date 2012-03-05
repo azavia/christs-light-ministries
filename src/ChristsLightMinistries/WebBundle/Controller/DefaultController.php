7979091e8471bb4fe5da14798e5e3702b6c751c9
Brandon Olivares <programmer2188@gmail.com>
2012-03-05 13:48:36 -0500

    Add base layout with new logo.
    Applied to index.html.twig and prayer-request.html.twig

--- src/ChristsLightMinistries/WebBundle/Controller/DefaultController.php
+++ src/ChristsLightMinistries/WebBundle/Controller/DefaultController.php
@@ -5,15 +5,17 @@ namespace ChristsLightMinistries\WebBundle\Controller;
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends ContainerAware
 {
     /**
+     * @Route("/{page}.html", defaults={"page" = "index"})
      * @Route("/", defaults={"page" = "index"})
-     * @Route("/{page}.html")
+     * @Method({"GET"})
      */
     public function indexAction($page)
     {
-        return $this->container->get('templating')->render("ChristsLightMinistriesWebBundle:Default:$page.html.twig");
+        return $this->container->get('templating')->renderResponse("ChristsLightMinistriesWebBundle:Default:$page.html.twig");
     }
 }
